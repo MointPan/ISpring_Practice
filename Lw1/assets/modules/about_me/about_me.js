034bb7f07ae78fef1af6482eb669d003c54c928f
MointPan <moint.pan3@gmail.com>
2021-05-27 16:41:09 +0300

    '$(document).ready moved to page's scrypts'

--- Lw1/assets/modules/about_me/about_me.js
+++ Lw1/assets/modules/about_me/about_me.js
@@ -1,14 +1,29 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import $ from 'jquery';
 
-// any CSS you import will output into a single css file (app.css in this case)
 import './about_me.css';
 
-// start the Stimulus application
-//import './bootstrap';
-
 import './components/slider/slider.js';
+
+$(document).ready(function () {
+    $('#next-button').click(function() {
+        nextSlide();
+    });
+
+    $('#prev-button').click(function() {
+        prevSlide();
+    });
+
+    $('.slide-nav-button').click(function() {
+        navButtonId = $(this).index();
+
+        if (navButtonId + 1 != slideNow) {
+            translateWidth = -$('#viewport').width() * (navButtonId);
+            $('#slidewrapper').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow = navButtonId + 1;
+        }
+    });
+});
--- Lw1/assets/modules/about_me/components/slider/slider.js
+++ Lw1/assets/modules/about_me/components/slider/slider.js
@@ -41,27 +41,3 @@ function prevSlide() {
         slideNow--;
     }
 }
-
-$(document).ready(function () {
-    $('#next-button').click(function() {
-        nextSlide();
-    });
-
-    $('#prev-button').click(function() {
-        prevSlide();
-    });
-
-    $('.slide-nav-button').click(function() {
-        navButtonId = $(this).index();
-
-        if (navButtonId + 1 != slideNow) {
-            translateWidth = -$('#viewport').width() * (navButtonId);
-            $('#slidewrapper').css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            slideNow = navButtonId + 1;
-        }
-    });
-});
