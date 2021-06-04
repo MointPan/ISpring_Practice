4bbdc31ae20d04b2936f7a1238c744545de59721
MointPan <moint.pan3@gmail.com>
2021-06-04 13:42:27 +0300

    Config fixed + Slider made as class to distribute prorties on every object

--- Lw1/assets/modules/about_me/about_me.js
+++ Lw1/assets/modules/about_me/about_me.js
@@ -2,28 +2,12 @@ import $ from 'jquery';
 
 import './about_me.css';
 
-import './components/slider/slider.js';
+import Slider from './components/slider/slider.js';
 
-$(document).ready(function () {
-    $('#next-button').click(function() {
-        nextSlide();
-    });
-
-    $('#prev-button').click(function() {
-        prevSlide();
-    });
-
-    $('.slide-nav-button').click(function() {
-        navButtonId = $(this).index();
-
-        if (navButtonId + 1 != slideNow) {
-            translateWidth = -$('#viewport').width() * (navButtonId);
-            $('#slidewrapper').css({
-                'transform': 'translate(' + translateWidth + 'px, 0)',
-                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-            });
-            slideNow = navButtonId + 1;
-        }
-    });
+$(document).ready(() => {
+    $('.slider').each((i, obj) => {
+        //Create new "slider" object for each slider on the page
+        const slider = new Slider($(obj));
+        slider.init();
+    })
 });
--- Lw1/assets/modules/about_me/components/slider/slider.js
+++ Lw1/assets/modules/about_me/components/slider/slider.js
@@ -1,43 +1,86 @@
 import $ from 'jquery';
 import './slider.css';
 
-var slideNow = 1;
-var slideCount = $('#slidewrapper').children().length;
-var slideInterval = 2000;
-var navButtonId = 0;
-var translateWidth = 0;
+export default class Slider {
+    constructor(object) {
+        //Slider compomemts
+        this._master = $(object);
+        this._viewport = $(object.children()[0]);
+        this._wrapper = $(this._viewport.children()[0]);
+        this._prev_button = $(this._viewport.children()[1]);
+        this._next_button = $(this._viewport.children()[2]);
+        this._nav_buttons = $(this._viewport.children()[3]).children();
 
-function nextSlide() {
-    if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
-        $('#slidewrapper').css('transform', 'translate(0, 0)');
-        slideNow = 1;
-    } else {
-        translateWidth = -$('#viewport').width() * (slideNow);
-        $('#slidewrapper').css({
-            'transform': 'translate(' + translateWidth + 'px, 0)',
-            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-        });
-        slideNow++;
+        //Slider parameters 
+        this._slideNow = 1;
+        this._slideCount = this._wrapper.children().length;
+        this._slideInterval = 2000;
+        this._navButtonId = 0;
+    }
+
+    init() {
+        this._listenPrevNextButtonsEvents();
+        this._listenNavButtonsEvents();
+    }
+
+    _listenPrevNextButtonsEvents() {
+        this._next_button.click(this._nextSlide.bind(this));
+        this._prev_button.click(this._prevSlide.bind(this));
+    }
+
+    _nextSlide() {
+        let translateWidth;
+        if (this._slideNow == this._slideCount || this._slideNow <= 0 || this._slideNow > this._slideCount) {
+            this._wrapper.css('transform', 'translate(0, 0)');
+            this._slideNow = 1;
+        } else {
+            translateWidth = -this._viewport.width() * (this._slideNow);
+            this._wrapper.css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            this._slideNow++;
+        }
+    }
+
+    _prevSlide() {
+        let translateWidth
+        if (this._slideNow == 1 || this._slideNow <= 0 || this._slideNow > this._slideCount) {
+            translateWidth = -this._viewport.width() * (this._slideCount - 1);
+            _wrapper.css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            this._slideNow = this._slideCount;
+        } else {
+            translateWidth = -this._viewport.width() * (this._slideNow - 2);
+            this._wrapper.css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            this._slideNow--;
+        }
+    }
+
+    _listenNavButtonsEvents() {
+        const that = this;
+        const Clicked = function() {
+            let translateWidth;
+            that._navButtonId = $(this).index();
+            if (that._navButtonId + 1 != that._slideNow) {
+                translateWidth = -that._viewport.width() * (that._navButtonId);
+                that._wrapper.css({
+                    'transform': 'translate(' + translateWidth + 'px, 0)',
+                    '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                    '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                });
+            that._slideNow = that._navButtonId + 1;
+            }
+        }
+        this._nav_buttons.click(Clicked);
     }
 }
 
-function prevSlide() {
-    if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
-        translateWidth = -$('#viewport').width() * (slideCount - 1);
-        $('#slidewrapper').css({
-            'transform': 'translate(' + translateWidth + 'px, 0)',
-            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-        });
-        slideNow = slideCount;
-    } else {
-        translateWidth = -$('#viewport').width() * (slideNow - 2);
-        $('#slidewrapper').css({
-            'transform': 'translate(' + translateWidth + 'px, 0)',
-            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
-            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
-        });
-        slideNow--;
-    }
-}
